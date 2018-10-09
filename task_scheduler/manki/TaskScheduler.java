bbe8518a21f4e650a1aa9f23526ab436b1ee8d11
manki.kim <manki.kim@navercorp.com>
2018-10-09 18:10:04 +0900

    #2 solve task schduler

---
+++ task_scheduler/manki/TaskScheduler.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * @author manki.kim
+ **/
+public class TaskScheduler {
+
+	public static void main(String... args) {
+		char[] tasks = {'A','A','A'
+			,'B', 'B','B'};
+
+		System.out.println(leastInterval(tasks, 2));
+	}
+
+	public static int leastInterval(char[] tasks, int n) {
+		if (tasks.length == 0)
+			return 0;
+		if (n < 0)
+			return 0;
+
+		Queue<Integer> jobs = new PriorityQueue<>();
+		Map<Character, Integer> taskIntervalMap = new HashMap<>();
+
+		for (char task : tasks) {
+			int taskInterval = taskIntervalMap.getOrDefault(task, 0);
+			jobs.add(taskInterval);
+			taskIntervalMap.put(task, taskInterval + n + 1);
+		}
+
+		int processingTime = 0;
+		while (!jobs.isEmpty()) {
+			int job = jobs.poll();
+
+			if (job <= processingTime) {
+				processingTime++;
+			} else {
+				processingTime += job - processingTime + 1;
+			}
+
+		}
+
+		return processingTime;
+	}
+}
