d38123c6cad2944d05dd841b0ab73379b9b25534
Yong Jae Kim <youthful2016@gmail.com>
2018-11-15 17:04:40 +0900

    solve house robber 3

---
+++ house_robber_3/yojkim/337.go
@@ -0,0 +1,21 @@
+func rob(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	sum := root.Val
+
+	if root.Left != nil {
+		sum += rob(root.Left.Left) + rob(root.Left.Right)
+	}
+
+	if root.Right != nil {
+		sum += rob(root.Right.Left) + rob(root.Right.Right)
+	}
+
+	childSum := rob(root.Left) + rob(root.Right)
+	if sum < childSum {
+		return childSum
+	} else {
+		return sum
+	}
+}
