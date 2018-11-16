c30ae3569dab987d6c2b6e5671afe90335b5cad1
SoYoung210 <ethdud1@gmail.com>
2018-11-17 00:10:18 +0900

    [#7] :rocket:  first-commit : Queue

---
+++ binary_tree_level_order_traversal/SY/Solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+	    if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while(size > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+                size--;
+            }
+            result.add(list);
+        }
+	    return result;
+    }
+}
