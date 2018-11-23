bb6038da2df5f2fb682e21d9ce080dfaf995967a
kim.taehyung <xodaktmdnj@naver.com>
2018-11-23 23:33:23 +0900

    #8 solve Shortest Unsorted Continuous Subarray

---
+++ shortest_unsorted_continuous_subarray/kth/ShortestUnsortedContinuousSubarray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine(new Solution().FindUnsortedSubarray((new int[] { 1,2,3,4 }))); 
+
+        }
+    }
+    public class Solution
+    {
+        public int FindUnsortedSubarray(int[] nums)
+        {
+            var sortedNums = (from num in nums orderby num select num).ToList();
+            var diff = sortedNums.Select((num, index) => new { Number = num, Index = index })
+                .Where(x => nums[x.Index] != x.Number).Select(x => x.Index).ToList();
+            return diff.Count() == 0 ? 0 : diff.Last() - diff.First() + 1;
+        }
+    }
+}
