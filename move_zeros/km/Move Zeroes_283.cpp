76b5907deef1017fdd4dcbb4c6fd8f5c77af5bdc
KM <sodp2@naver.com>
2018-12-08 16:36:18 +0900

    #11 solve move zeroes

---
+++ move_zeros/km/Move Zeroes_283.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	void moveZeroes(vector<int>& nums) {
+		vector<int> result;
+		int sub_count;
+
+		for (int &num : nums)
+		{
+			if (num != 0)
+				result.push_back(num);
+		}
+
+		sub_count = nums.size() - result.size();
+
+		for (auto i = 0; i < sub_count; ++i)
+		{
+			result.push_back(0);
+		}
+
+		nums = result;
+	}
+};
+
+void trimLeftTrailingSpaces(string &input) {
+	input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+		return !isspace(ch);
+	}));
+}
+
+void trimRightTrailingSpaces(string &input) {
+	input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+		return !isspace(ch);
+	}).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+	vector<int> output;
+	trimLeftTrailingSpaces(input);
+	trimRightTrailingSpaces(input);
+	input = input.substr(1, input.length() - 2);
+	stringstream ss;
+	ss.str(input);
+	string item;
+	char delim = ',';
+	while (getline(ss, item, delim)) {
+		output.push_back(stoi(item));
+	}
+	return output;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+	if (length == -1) {
+		length = list.size();
+	}
+
+	if (length == 0) {
+		return "[]";
+	}
+
+	string result;
+	for (int index = 0; index < length; index++) {
+		int number = list[index];
+		result += to_string(number) + ", ";
+	}
+	return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+	string line;
+	while (getline(cin, line)) {
+		vector<int> nums = stringToIntegerVector(line);
+
+		Solution().moveZeroes(nums);
+
+		string out = integerVectorToString(nums);
+		cout << out << endl;
+	}
+	return 0;
+}
