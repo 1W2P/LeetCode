2cdbc669d43a8eaba94648a93ec6a410c808775e
Soyeon Kim <ese2003@naver.com>
2018-12-08 12:46:26 +0900

    solve move zeros

---
+++ move_zeros/soyeon/move_zeros.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+      int cnt = 0;
+
+      while (find(nums.begin(), nums.end(), 0) != nums.end() ) {
+        nums.erase(find(nums.begin(), nums.end(), 0));
+        cnt++;
+      }
+
+      for(int i=0; i<cnt; i++) {
+        nums.push_back(0);
+      }
+    }
+};
+
+int main() {
+  vector<int> a = {0,1,0,3,12};
+  Solution sol;
+  sol.moveZeroes(a);
+}
