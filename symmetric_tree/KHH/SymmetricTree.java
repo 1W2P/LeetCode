393b22bb5cfd761a283222284d2b70f07381e37d
hyehyeonK <>
2018-12-14 23:27:31 +0900

    #11 solve Symmetric Tree

---
+++ symmetric_tree/KHH/SymmetricTree.java
@@ -0,0 +1,103 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+    public boolean isSymmetric(TreeNode root) {
+        if(null == root) return true;
+        else return isEqule(root.left,  root.right);
+    }
+    public static boolean isEqule(TreeNode left, TreeNode right)
+    {
+        if (null == left)
+        {
+            if (null == right) return true;
+            else return false;
+        }
+        else
+        {
+            if( null == right) 
+                return false;
+            else if(left.val == right.val)
+            {
+                return isEqule(left.left, right.right) && isEqule(left.right, right.left);
+            }
+            else return false;
+        }
+    }
+}
+
+
+/* 
+* Main 
+*/
+public class SymmetricTree {
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+    
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+    
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+    
+            if (index == parts.length) {
+                break;
+            }
+    
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+    
+            if (index == parts.length) {
+                break;
+            }
+    
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+    
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+            
+            boolean ret = new Solution().isSymmetric(root);
+            
+            String out = booleanToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
