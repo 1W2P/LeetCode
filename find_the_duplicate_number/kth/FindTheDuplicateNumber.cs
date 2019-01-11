060dd07e6a1d52cbf673d4e9a88c80b08038c090
kim.taehyung <xodaktmdnj@naver.com>
2019-01-12 01:30:44 +0900

    #14 Solve Find the duplicateNumber

---
+++ find_the_duplicate_number/kth/FindTheDuplicateNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+    public class Solution
+    {
+        public int FindDuplicate(int[] nums)
+        {
+            var uniq = nums.Distinct();
+            return (nums.Sum() - uniq.Sum())/(nums.Count() - uniq.Count());
+        }
+    }
+
+}
