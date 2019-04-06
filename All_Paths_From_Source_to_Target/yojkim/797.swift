08bed4bb439c72d254fe3c7ba052197f689ebc27
Yong Jae Kim <youthful2016@gmail.com>
2019-04-06 20:49:01 +0900

    update no.797

--- All_Paths_From_Source_to_Target/yojkim/797.swift
+++ All_Paths_From_Source_to_Target/yojkim/797.swift
@@ -3,17 +3,14 @@ import Foundation
 class Solution {
     
     var matrix: [[Bool]]?
-    var isVisited: [[Bool]]?
     
     func searchPath(currentRow: Int) -> [[Int]] {
-        guard var matrix = self.matrix,
-            var isVisited = self.isVisited else { return [] }
+        guard var matrix = self.matrix else { return [] }
         
         var result: [[Int]] = []
         
         for (idx, isValid) in matrix[currentRow].enumerated() {
             if isValid {
-                isVisited[currentRow][idx] = true
                 let returnPath = self.searchPath(currentRow: idx)
                 for path in returnPath {
                     var retPath = path
@@ -38,15 +35,6 @@ class Solution {
             ), count: graph.count
         )
         
-        self.isVisited = Array(
-            repeating: Array(
-                repeating: false,
-                count: graph.count
-            ), count: graph.count
-        )
-        
-        var result: [[Int]] = []
-        
         graph.enumerated().forEach {
             let (idx, values) = $0
             values.forEach {
@@ -54,19 +42,6 @@ class Solution {
             }
         }
         
-        if let first = matrix?.first {
-            for (idx, isValid) in first.enumerated() {
-                if isValid {
-                    let returnPath = self.searchPath(currentRow: idx)
-                    for path in returnPath {
-                        var retPath = path
-                        retPath.insert(0, at: 0)
-                        result.append(retPath)
-                    }
-                }
-            }
-        }
-        
-        return result
+        return self.searchPath(currentRow: 0)
     }
 }
